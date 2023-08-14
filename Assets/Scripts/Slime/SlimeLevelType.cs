0ccfb49f24101135537c2c47b5a7153bf47fa2df
Danila <belkin.job123@gmail.com>
2023-08-14 15:08:09 +0300

    add Slime logic

---
+++ Assets/Scripts/Slime/SlimeLevelType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Slime
+{
+    [CreateAssetMenu(fileName = "NewLevel", menuName = "Custom/SlimeLevel", order = 51)]
+    public class SlimeLevelType : ScriptableObject
+    {
+        [SerializeField] private SlimeSet _set;
+        [SerializeField] private GameObject _enviroment;
+
+        public SlimeSet SlimeSet => _set;
+        public GameObject Enviroment => _enviroment;
+    }
+}
---
+++ Assets/Scripts/Slime/SlimeSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Slime
+{
+    
+    [CreateAssetMenu(fileName = "NewSlimeSet", menuName = "Custom/SlimeSet", order = 51)]
+    public class SlimeSet : ScriptableObject
+    {
+        [SerializeField] private List<Slime> _slimes;
+
+        public int Size => _slimes.Count;
+
+        private void Awake()
+        {
+            for (var i = 0; i < _slimes.Count; i++)
+            {
+                _slimes[i].SetID(i);
+            }
+        }
+
+        public Slime GetAnimalTemplate(int index)
+        {
+            return _slimes[index];
+        }
+    }
+}
