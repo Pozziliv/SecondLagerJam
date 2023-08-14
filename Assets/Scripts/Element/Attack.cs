3429509f8f2242ab1f6f883fc9da644768d7ef52
Danila <belkin.job123@gmail.com>
2023-08-14 17:52:36 +0300

    Add fight scripts

---
+++ Assets/Scripts/Element/Attack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Element
+{
+    public class SlimeAttack : MonoBehaviour
+    {
+
+
+        
+        public void GetDamage()
+        {
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Element/Element.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Element
+{
+    public class Element : MonoBehaviour
+    {
+
+        enum Name
+        {
+            Neutral = 0,
+            Fire = 1,
+            Wood = 2,
+            Water = 3,
+        }
+
+
+        
+    }
+}
