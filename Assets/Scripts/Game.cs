59efd7089970afd4148ac62318fef77df0e4f659
Danila <belkin.job123@gmail.com>
2023-08-16 01:32:34 +0300

    я пошел спать

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -181,7 +181,7 @@ public class Game : MonoBehaviour
                 _plusText.transform.position = Camera.main.WorldToScreenPoint(_lastAviary.DoorPosition + _lastAviary.transform.forward * 2.5f);
             else
                 _plusText.transform.position = _lastAviary.DoorPosition + _lastAviary.transform.forward * 3.5f + _lastAviary.transform.up * 4.5f;
-            print($"DoneCombo: {score}");
+            
             _plusText.Show("+" + score.ToString());
         }
         StartCoroutine(MovePlusText(_plusText, 0.4f, score));
